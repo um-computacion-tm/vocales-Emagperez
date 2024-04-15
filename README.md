88cfc162f9902197b2aafb30cf50c3b03e5b3ef5
emagperez <eg.perez@alumno.um.edu.ar>
2024-04-15 20:39:27 -0300

    Contador Vocales

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/95SjdgC7)
+
+Nombre: Emanuel Perez Gianolini
+Legajo: 60117
+DNI: 42.795.175
+
+
+Este Repositorio cuenta con dos archivos, uno que contiene la funcion de contar vocales y otro con test que verifica que funcione correctamente la funcion.
+En cuanto al primer archivo,Toma una cadena de texto como entrada y devuelve un diccionario que contiene la cantidad de cada vocal presente en la palabra, para realizarlo primero la funcion convierte la palabra en minuscula. Luego, itera sobre cada letra en la palabra ,comprueba si la letra es la vocal e incrementa el contador de esa vocal en el diccionario de resultados.
+
+
+Y en cuanto al segundo archivo tenemos test que comprueba el resultado de palabras sin vocales, con vocal unica , con vocales repetias, con muchas vocales diferentes y despues con palabras mayusculas
---
+++ Test_Contador_Vocales.py
@@ -0,0 +1,108 @@
+import unittest
+
+from Contadorvocales import contar_vocales
+
+
+class TestContarVocales(unittest.TestCase):
+
+    def test_sin_vocales(self):
+
+        palabra = "tryhgf"
+
+        resultado = contar_vocales(palabra)
+
+        self.assertEqual(resultado, {})
+
+
+
+    def test_con_vocal_o(self):
+
+        palabra = "sol"
+
+        resultado = contar_vocales(palabra)
+
+        self.assertEqual(resultado, {"o": 1})
+
+
+
+    def test_con_vocal_doble_o(self):
+
+        palabra = "solo"
+
+        resultado = contar_vocales(palabra)
+
+        self.assertEqual(resultado, {"o": 2})
+
+
+
+    def test_con_dos_vocales(self):
+
+        palabra = "sola"
+
+        resultado = contar_vocales(palabra)
+
+        self.assertEqual(resultado, {"o": 1, "a": 1})
+
+
+
+    def test_con_todas_las_vocales(self):
+
+        palabra = "solamente quiero que gane Boca"
+
+        resultado = contar_vocales(palabra)
+
+        self.assertEqual(
+
+            resultado,
+
+            {"a": 3, "e": 5, "i": 1, "o": 3, "u": 2},
+
+        )
+
+
+
+    def test_con_vocales_en_mayuscula(self):
+
+        palabra = "SOlAmente quIerO"
+
+        resultado = contar_vocales(palabra)
+
+        self.assertEqual(
+
+            resultado,
+
+            {"a": 1, "e": 3, "i": 1, "o": 2, "u": 1},
+
+        )
+
+
+    def test_con_vocales_en_mayuscula_2(self):
+
+        palabra = "Emanuel Perez"
+
+        resultado = contar_vocales(palabra)
+
+        self.assertEqual(
+
+            resultado,
+
+            {"a": 1, "e": 4,"u": 1},
+
+        )
+
+    def test_con_vocales_en_mayuscula_3(self):
+
+        palabra = "Universidad de Mendoza"
+
+        resultado = contar_vocales(palabra)
+
+        self.assertEqual(
+
+            resultado,
+
+            {"a": 2 ,"e": 3,"i" : 2,"o":1,"u": 1},
+
+        )
+
+if __name__ == '__main__':
+    unittest.main()
